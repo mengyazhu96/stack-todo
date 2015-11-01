caa2713fc8e77ae2dbddfe3d22d9d2c32e4a2076
mengyazhu96@gmail.com <mengyazhu96@gmail.com>
2015-10-31 20:17:54 -0400

    include files

---
+++ todoItem.js
@@ -0,0 +1,27 @@
+(function() { 
+	// set up the controller for the accounts display
+	var controller = function($scope) {
+		$scope.toDo = [
+			{
+				'text': 'CS pset'
+			},
+			{
+				'text': 'stat pset'
+			}
+		]
+	};
+
+	controller.$inject = ['$scope'];
+
+	// set up the accounts display directive
+	var directive = function() {
+        return {
+            restrict: 'E',
+            templateUrl: 'todo-item.html',
+            controller: controller
+        };
+    };
+
+    angular.module('todoItem',[])
+    .directive('todoItem',directive)
+})();
