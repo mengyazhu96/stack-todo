baea23c6d4fdc23368d257d3ff5f805e47105528
mengyazhu96@gmail.com <mengyazhu96@gmail.com>
2015-10-31 21:27:31 -0400

    some view stuff

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 (function() {
 
 	// initialize app
-	angular.module('app',['todoItem','addItem']);
+	var app = angular.module('app',['todoItem','addItem']);
+
+	var controller = function($scope) {
+		$scope.showInput = true;
+		$scope.changeInput = function() {
+			$scope.showInput = !$scope.showInput;
+		}
+	}
+
+	controller.$inject = ['$scope'];
+
+	app.controller('ctrl',controller);
 
 })();
